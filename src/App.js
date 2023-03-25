7bad107bb47d970da2ea56d7a7d4e90bb4daa78b
shivang <shivangmathur101@gmail.com>
2023-03-25 23:00:24 +0530

    implemented Bar graph

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import BarChart from "./components/BarChart";
+import { useState } from "react";
+import { UserData } from "./Data";
 
 function App() {
+
+const [cd, setChartData] = useState({
+
+  labels: UserData.map((item) => item.year),
+  datasets: [{
+    label: "User Gain",
+    data: UserData.map((item) => item.userGain),
+    backgroundColor: "rgba(255, 99, 132, 0.2)",
+    borderColor: "rgba(255, 99, 132, 1)",
+    borderWidth: 1,
+  }]
+})
+
   return (
+    <>
     <h1 className="text-3xl font-bold underline">
     Hello world!
   </h1>
+  <div className="w-2/4">
+  < BarChart chartValues={cd} />
+
+  </div>
+    </>
   );
 }
 
